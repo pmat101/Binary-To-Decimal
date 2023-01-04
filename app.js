ac9e5e2407bd90b2c9aa094d761a2a07a6bd86aa
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-01-04 21:13:57 +0530

    condition added to check if entered number isn't binary

--- app.js
+++ app.js
@@ -6,11 +6,20 @@ document.querySelector("form").addEventListener("submit", function (e) {
 });
 
 document.querySelector(".container .btn").addEventListener("click", function () {
-    let binary = document.querySelector("#binary").value;
-    document.querySelector(".container .row .result").innerText = "Decimal Number : " + parseInt(binary, 2);
+    let flag = 0;
+    let binary = document.getElementById("binary").value;
+    for (let i = 0; i < binary.length; i++) {
+        if ((binary[i] != 0) && (binary[i] != 1)) {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 1)
+        document.querySelector(".container .row .result").innerText = "Please enter a valid binary number!";
+    else
+        document.querySelector(".container .row .result").innerText = "Decimal Number : " + parseInt(binary, 2);
     /*
         A radix parameter specifies the number system to use:
-
         2 = binary, 8 = octal, 10 = decimal (default), 16 = hexadecimal.
     */
 });
